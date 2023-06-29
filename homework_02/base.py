eb7ac82fd37464e6309aa38a397920bb260679cb
varusN <art.varakin@gmail.com>
2023-06-29 18:22:23 +0300

    homework_04, changed config

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,16 +1,15 @@
 from abc import ABC
 from homework_02.exceptions import LowFuelError, NotEnoughFuel
 
-class Vehicle(ABC):
 
-    def __init__(self,weight,fuel,fuel_consumption,started):
+class Vehicle(ABC):
+    def __init__(self, weight, fuel, fuel_consumption, started):
         self.weight = weight
         self.fuel = fuel
         self.fuel_consumption = fuel_consumption
         self.started = started
 
-
-    def start(self,started,fuel):
+    def start(self, started, fuel):
         if started != 1:
             try:
                 fuel > 0
@@ -19,14 +18,12 @@ class Vehicle(ABC):
             else:
                 self.started = 1
 
-
-    def move(self,dist):
+    def move(self, dist):
         fuel = self.fuel
         fuel_consumption = self.fuel_consumption
         try:
-            dist <= fuel/fuel_consumption
+            dist <= fuel / fuel_consumption
         except NotEnoughFuel:
             print()
         fuel -= dist * fuel_consumption
         return fuel
-
