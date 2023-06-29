189454c66bd5f565ad92758b0890a1d537b9d959
varusN <art.varakin@gmail.com>
2023-06-29 18:20:26 +0300

    homework_04, changed config

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -4,6 +4,9 @@
 
 from aiohttp import ClientSession
 
+USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
+POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
+
 async def get_data(url: str):
     async with ClientSession() as session:
         async with session.get(url) as response:
